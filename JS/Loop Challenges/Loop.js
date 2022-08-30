b19f088b4c0badf3be3a80a34c5ed72f56b29e88
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-08-30 14:52:04 +0300

    ready

--- JS/Loop Challenges/Loop.js
+++ JS/Loop Challenges/Loop.js
@@ -4,6 +4,7 @@ for(var i=1;i<=20;i++){
     }
 }
 
+
 for(var i=100;i>=0;i--){
     if(i%3==0){
         console.log(i)
@@ -16,6 +17,12 @@ for(var i=4; i>=-3.5;i-=1.5){
 
 var sum=0;
 for(var i=1;i<=100;i++){
-    sum+=i;
+    sum+=i; 
 }
 console.log(sum)
+
+var factorial=1;
+for(var i=1; i<=12 ; i++){
+    factorial*=i;
+}
+console.log(factorial)

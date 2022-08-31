8a3159fc7bffe73780ead7076076bc3bb42ebb6a
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-08-31 14:08:26 +0300

    first-part

--- JS/profile-page/script.js
+++ JS/profile-page/script.js
@@ -1,20 +1,18 @@
 console.log("page loaded...");
 
-var nameChange = document.querySelector ("#name1");
-var deletReq = document.querySelector ("#connection1");
-var deletReq = document.querySelector ("#connection2");
-
-
-
 function edit(){
+    var nameChange = document.querySelector ("#name1");
     nameChange.innerText= "Somebody";
 }
 
 
 function accept1(){
+    const deletReq = document.querySelector (".card-list-item");
     deletReq.remove();
 }
 
-function cancel1(){
-    deletReq.remove();
+
+function subNum(){
+    var connNum = document.querySelector (".badge")
+    connNum.innerText--;
 }

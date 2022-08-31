210bd66dc979ae481d53cbd709790a13ec360c6d
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-08-31 20:19:25 +0300

    part1

--- JS/Dojo Pizzeria/script.js
+++ JS/Dojo Pizzeria/script.js
@@ -1,10 +1,9 @@
-function pizzaOven(crustType,sauceType,cheeses,toppings){
+function pizzaOven(crustype,sauceType,cheeses,toppings){
     var pizza = {};
     pizza.crustType= crustType;
     pizza.sauceType= sauceType;
     pizza.cheeses= cheeses;
     pizza.toppings= toppings;
-
     return pizza;
 }
 
---
+++ JS/DojoWeather/script.js
@@ -0,0 +1,9 @@
+function dismissCookie(){
+    var cookieElement =  document.querySelector("footer");
+    cookieElement.remove();
+}
+
+// function convFahrenheit(){
+//     var celsiusDegree = document.querySelector(".dayDegree");
+//     celsiusDegree.innerText= 
+// }

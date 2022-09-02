df034011567664a58b23f2108fbeb2f1e6888077
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-02 07:57:53 +0300

    ready

--- JS/Likes/Part 2/script.js
+++ JS/Likes/Part 2/script.js
@@ -1,22 +1,25 @@
 var count = 0;
-var countElement= document.querySelector ("#count1");
+
 
 console.log(countElement);
 
 function add1(){
-    count++;
-    countElement.innerText = count + "like(s)";
-    console.log(count);
+    var countElement= document.querySelector ("#count1");
+    countElement.innerText++;
 }
 
 
 var count2 = 0;
-var countElement2= document.querySelector ("#count2");
 
 console.log(countElement2);
 
 function add2(){
-    count2++;
-    countElement.innerText = count2 + "like(s)";
-    console.log(count2);
+    var countElement2= document.querySelector ("#count2");
+    countElement2.innerText++;
+}  
+
+
+function add3(){
+    var countElement3= document.querySelector ("#count3");
+    countElement3.innerText++;
 }  

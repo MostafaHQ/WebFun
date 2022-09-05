1d23e1c864664d78256a6f0daf539f20513902f4
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-05 16:55:53 +0300

    ready

--- JS/4 Function Calculator/script.js
+++ JS/4 Function Calculator/script.js
@@ -1,16 +1,45 @@
-function press(){
-    var number = document.querySelector("#display");
-    number.innerText = 
+let currNum = "";
+let numOne = "";
+let oper = "";
+
+let disp = document.querySelector("#display");
+
+function press(num) {
+  currNum += num;
+  console.log(currNum);
+  disp.innerText = currNum;
 }
 
-function setOP(){
-
+function setOP(op) {
+  oper = op;
+  numOne = currNum;
+  currNum = "";
+  disp.innerText = "0";
 }
 
-function calculate(){
-
+function clr() {
+  currNum = "";
+  numOne = "";
+  disp.innerText = "0";
+  oper = "";
 }
+function calculate() {
+  let res;
+  if (oper === "+") {
+    res = parseFloat(numOne) + parseFloat(currNum);
+  }
+  disp.innerText = res;
+  if (oper === "-") {
+    res = parseFloat(numOne) - parseFloat(currNum);
+  }
+  disp.innerText = res;
 
-function clr(){
-
+  if (oper === "*") {
+    res = parseFloat(numOne) * parseFloat(currNum);
+    disp.innerText = res;
+  }
+  if (oper === "/") {
+    res = parseFloat(numOne) / parseFloat(currNum);
+    disp.innerText = res;
+  }
 }

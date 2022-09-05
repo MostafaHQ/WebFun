30e28d3dd7ee6e99081e768e59f1283505ed0685
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-05 15:35:20 +0300

    -

---
+++ JS/4 Function Calculator/script.js
@@ -0,0 +1,16 @@
+function press(){
+    var number = document.querySelector("#display");
+    number.innerText = 
+}
+
+function setOP(){
+
+}
+
+function calculate(){
+
+}
+
+function clr(){
+
+}
--- JS/DojoWeather/script.js
+++ JS/DojoWeather/script.js
@@ -3,49 +3,20 @@ function dismissCookie() {
     cookieElement.remove();
 }
 
-// function changeDegree(element) {
-//     var x = parseInt(document.querySelectorAll(".dayDegree").innerHtml);
-//     var y = parseInt(document.querySelectorAll(".nightDegree").innerHtml);
-//     if (element.value == "°C") {
-//         ctof(x, y);
-//     } else {
-//         ftoc(x, y);
-//     }
-// }
-
-// function ctof(x, y) {
-//     for (var i = 0; i < x.length; i++) {
-//         x[i].innerHtml = (x[i] * 9 / 5) + 32;
-//     }console.log(x)
-//     for (var j = 0; j < x.length; j++) {
-//         y[j].innerHtml = (y[j] * 9 / 5) + 32;
-//     }
-// }
-
-// function ftoc(x, y) {
-//     for (var i = 0; i < x.length; i++) {
-//         x[i].innerHtml = (x[i] - 32) * 5 / 9;
-//     }
-//     for (var j = 0; j < x.length; j++) {
-//         y[j].innerHtml = (y[j] - 32) * 5 / 9;
-//     }
-// }
-
-
 function changeDegree() {
 
     var degrees = document.querySelectorAll("#tempDegree");
 
     var selectedValue = document.getElementById("temperature").value;
 
-    if (selectedValue =="°F") {
+    if (selectedValue == "°F") {
 
         for (var i = 0; i < degrees.length; i++) {
 
             degrees[i].innerText = Math.round(parseInt(degrees[i].innerText) * (9 / 5) + 32);
         }
     }
-    else if (selectedValue =="°C"){
+    else if (selectedValue == "°C") {
 
         for (var i = 0; i < degrees.length; i++) {
 
